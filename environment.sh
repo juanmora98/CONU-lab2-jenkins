c6aecb472f3055e9e07345d63aa59cfed923cca1
juan felipe mora <43444251+juanmora98@users.noreply.github.com>
2019-11-08 00:07:56 -0500

    Add files via upload

---
+++ environment.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Setting environment variables for Terraform"
+export ARM_SUBSCRIPTION_ID=2a6bdf1f-cab8-4c0d-8887-9bba196f5df3
+export ARM_CLIENT_ID=40388d90-b3ea-4abc-936a-5111710101d9
+export ARM_CLIENT_SECRET=9a47ae56-8bf2-41cf-9b29-e176e0c1497f
+export ARM_TENANT_ID=50640584-2a40-4216-a84b-9b3ee0f3f6cf
+export ARM_ENVIRONMENT=public
